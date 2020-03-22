a87196a5603e9dcf02e7c7d1a72694f50958378d
657463659@qq.com <@WZX1314520my>
2020-03-22 18:24:52 +0800

    springboot整合dubbo与zookeeper

--- README.md
+++ README.md
@@ -8,3 +8,5 @@ gmall-api  项目中各种版本和bean
 gmall-common-util 通用工具依赖包 (spring-boot-starter-web,fastjson,httpclient,apache工具包)
 gmall-service-uitl  web后端(会引入通用工具包)service工具包依赖(jdbc、mybatis、jedis、activemq)
 gmall-web-util      web前端(会引入通用工具包)controller工具包依赖(Thymeleaf)
+
+springboot整合dubbo(2.7.3)+zookeeper(3.4.13):https://blog.csdn.net/wangzibai/article/details/100106261
--- gmall-user-service/src/main/java/com/example/gmall/user/service/service/impl/UserServiceImpl.java
+++ gmall-user-service/src/main/java/com/example/gmall/user/service/service/impl/UserServiceImpl.java
@@ -8,8 +8,8 @@ import com.example.gmall.service.UserService;
 
 import com.example.gmall.user.service.mapper.UmsMemberReceiveAddressMapper;
 import com.example.gmall.user.service.mapper.UserMapper;
+import org.apache.dubbo.config.annotation.Service;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
