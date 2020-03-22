187f422e7679d4b96f9e756efc7be31f084a7068
657463659@qq.com <@WZX1314520my>
2020-03-22 13:20:01 +0800

    提交pom

--- gmall-user-service/src/main/java/com/example/gmall/user/service/GmallUserServiceApplication.java
+++ gmall-user-service/src/main/java/com/example/gmall/user/service/GmallUserServiceApplication.java
@@ -2,10 +2,8 @@ package com.example.gmall.user.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
-@MapperScan(basePackages = "com.example.gmall.user.service.mapper")
 public class GmallUserServiceApplication {
 
     public static void main(String[] args) {
